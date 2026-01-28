4d01460af93227271e0e6ff18be5597d1b37c10a
Qiao <qiaoxinang@gmail.com>
2026-01-28 22:52:30 +0800

    sample usage

--- analysis/export_results_to_excel.py
+++ analysis/export_results_to_excel.py
@@ -9,7 +9,14 @@ Creates:
 Usage:
   python analysis/export_results_to_excel.py --results_root "C:\\path\\to\\results\\advanced\\fbnetgen_v2"
   python analysis/export_results_to_excel.py --results_root "C:\\path\\to\\results\\advanced\\fbnetgen_v2" --output_dir "C:\\path\\to\\excel"
-"""
+
+  python analysis/export_results_to_excel.py \
+  --results_root "C:\Users\UltimateGirl\Documents\Qiao Xin\FIT3161 FYP\GNN-mri\results\advanced\fbnetgen_v2" \
+  --single_workbook \
+  --folds_dir "C:\Users\UltimateGirl\Documents\Qiao Xin\FIT3161 FYP\GNN-mri\data\folds_data" \
+  --csv_path "C:\Users\UltimateGirl\Documents\Qiao Xin\FIT3161 FYP\GNN-mri\data\ListSort_AgeAdj.csv"
+
+  """
 
 import argparse
 import json
